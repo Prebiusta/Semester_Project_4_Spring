dadc6bc708c37cf92bbbf4f2c583e6eac610a6fd
Kwoconut <43205572+Kwoconut@users.noreply.github.com>
2020-05-21 09:23:12 +0200

    Setter and getter to DimensionTimeOfDayEntity
    DimensionTimeOfDayEntity now can be used! :)

--- src/main/java/en/via/SEP4/WareHouseModel/DimensionTimeOfDayEntity.java
+++ src/main/java/en/via/SEP4/WareHouseModel/DimensionTimeOfDayEntity.java
@@ -1,8 +1,15 @@
 package en.via.SEP4.WareHouseModel;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "D_TimeOfDay",schema = "archive_warehouse")
 public class DimensionTimeOfDayEntity {
