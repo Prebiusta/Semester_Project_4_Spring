ff0feb0cc235b61b15f46986c6971e89556076c3
David Franko <david.franko1998@gmail.com>
2020-05-29 10:26:31 -0700

    Fixed error when getting non existing archive

--- src/main/java/en/via/SEP4/DAO/DatabaseDAO/ArchiveDao.java
+++ src/main/java/en/via/SEP4/DAO/DatabaseDAO/ArchiveDao.java
@@ -4,5 +4,4 @@ import en.via.SEP4.Model.DBModel.ArchiveEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArchiveDao extends JpaRepository<ArchiveEntity, Long> {
-    ArchiveEntity findArchiveById(Long archiveId);
 }
--- src/main/java/en/via/SEP4/Service/ArchiveServiceImpl.java
+++ src/main/java/en/via/SEP4/Service/ArchiveServiceImpl.java
@@ -2,6 +2,7 @@ package en.via.SEP4.Service;
 
 import en.via.SEP4.DAO.DatabaseDAO.ArchiveDao;
 import en.via.SEP4.Model.DBModel.ArchiveEntity;
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +24,6 @@ public class ArchiveServiceImpl implements ArchiveService {
 
     @Override
     public ArchiveEntity getArchiveByArchiveId(Long archiveId) {
-        return archiveDao.findArchiveById(archiveId);
+        return archiveDao.findById(archiveId).orElseThrow(() -> new ResourceNotFoundException("ArchiveId " + archiveId + " not found"));
     }
 }
